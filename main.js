62f6c131960a372f08d8ca5807fdda213b196e9b
taomaru <jackson_maria1@columbusstate.edu>
2022-06-29 21:35:40 -0400

    Create a 16x16 grid of 100x100px square divs

--- main.js
+++ main.js
@@ -1,10 +1,25 @@
 const gridContainer = document.querySelector(".container");
 
+function makeRow() {
+    for(let i = 0; i < 16; i++) {
+       let newBox = document.createElement("div");
+       gridContainer.appendChild(newBox);
+       newBox.classList.add("gridDiv");
+    }
+}
+
+//makeRow();
+
 function makeGrid() {
     for(let i = 0; i < 16; i++) {
-        let newBox = document.createElement("div");
-        gridContainer.appendChild(newBox);
-        newBox.classList.add("gridDiv");
+        let newCol = document.createElement("div");
+        for(let aNum = 0; aNum < 16; aNum++) {
+            let aBox = document.createElement("div");
+            newCol.appendChild(aBox);
+            aBox.classList.add("gridDiv");
+        }
+        gridContainer.appendChild(newCol);
+        newCol.classList.add("gridColumn");
     }
 }
 
